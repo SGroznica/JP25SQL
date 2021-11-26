d8bf89a28750d7ec70a2b5c99033baee763660fb
SGroznica <93283616+SGroznica@users.noreply.github.com>
2021-11-26 21:24:08 +0100

    Dodana DZ od 24.11.2021.

--- zadaca.sql
+++ zadaca.sql
@@ -25,8 +25,32 @@ values (null,'Donji Miholjac','HRV','Beli Manastir','500.00');
 
 select * from city where CountryCode='HRV';
 
-update city set District='Osijek-Baranja' where ID=4081;
+update city set District='Osijek-Baranja' where ID=4080;
 
-update city set Name='Spickovina' where ID=4081;
+update city set Name='Spickovina' where ID=4080;
 
-delete from city where ID=4081;
+delete from city where ID=4080;
+
+#knjiznica nova DZ
+
+select a.naslov, b.aktivan
+from katalog a inner join izdavac b on a.izdavac = b.sifra
+where b.aktivan='0';
+
+select b.ime, b.prezime, a.naslov
+from katalog a inner join autor b on a.autor = b.sifra
+where a.naslov not like '%b%';
+
+select * from izdavac;
+
+select b.naziv, b.aktivan
+from katalog a inner join izdavac b on a.izdavac = b.sifra
+left join mjesto c on a.mjesto = c.sifra
+where c.postanskiBroj='10000';
+
+#sakila
+
+select b.country, a.city, c.address, c.address2 
+from city a inner join country b on a.country_id = b.country_id
+left join address c on c.city_id = a.city_id
+where address2 is null;
